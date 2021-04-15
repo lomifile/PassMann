009172100adcb5172d21c4524ca4ffc572c3d93b
Filip <fivanusec@gmail.com>
2021-04-15 11:09:09 +0200

    Installer update
    Started selected build testing from RPM build

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-cmake -B build
+if [ "$1" == "deb" ]; then
+   cmake -DDEB_PACK=ON -DRPM_PACK=OFF build
+elif [ "$1" == "rpm" ]; then
+   cmake -DRPM_PACK=ON -DDEB_PACK=OFF build
+else
+   cmake -B build
+fi
+
 cd build
 make
 make package
