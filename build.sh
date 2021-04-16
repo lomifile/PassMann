719dd2dcb4c43a4a474ca19d08f794ecfcc82850
Filip <fivanusec@gmail.com>
2021-04-16 08:57:29 +0200

    Revert last update

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
-RED="\e[31m"
+ED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
-if [ "$1" == "deb" ]; then
-   cmake -DDEB_PACK=ON -DRPM_PACK=OFF build
-elif [ "$1" == "rpm" ]; then
-   cmake -DRPM_PACK=ON -DDEB_PACK=OFF build
-else
-   cmake -B build
-fi
-
+cmake -B build
 cd build
 make
 make package
